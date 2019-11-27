609662f4d7c31c0d9366daa9916aa2f64ef25bc5
yaroslaviy <yaselokeev@gmail.com>
2019-11-27 12:01:50 -0500

    added proxy support

--- adidasspoofbrowser.js
+++ adidasspoofbrowser.js
@@ -2,46 +2,53 @@ const puppeteer = require('puppeteer');
 const log4js = require('log4js');
 const logger = log4js.getLogger();
 logger.level = 'info';
+const {parentPort, workerData} = require('worker_threads');
 
-
+const proxy = workerData.proxy;
 
 async function run() {
-  const browser = await puppeteer.launch({
-      headless: false,
-      args: ['--no-sandbox', 
-      '--disable-setuid-sandbox',
-      '--ignore-certifcate-errors',
+    const args = ['--no-sandbox', '--disable-setuid-sandbox', '--ignore-certifcate-errors'];
+    if(proxy.length > 0){
+    const arr_proxy = proxy.split(':')
+    const proxyserver = arr_proxy[0] + ':' + arr_proxy[1]
+    args.push(`--proxy-server=${proxyserver}`);
+    }
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: args,
 
-    ],
-    
-      defaultViewport: null
-  });
-  // open new tab
-  const page = await browser.newPage();
+        defaultViewport: {
+            width: 1200,
+            height: 1000
 
-  
-
-  await page.evaluateOnNewDocument(() => {
-    Object.defineProperty(navigator, 'webdriver', {
-      get: () => undefined,
+        }
     });
-  });
-  
+    // open new tab
+    const page = await browser.newPage();
 
-  //open nike
-  await page.goto('https://www.adidas.ca/yeezy');
-  
-  await page.waitForSelector('body', {visible: true});
-  await logger.info('opened adidas page  -', page.url());
+    await page.evaluateOnNewDocument(() => {
+        Object.defineProperty(navigator, 'webdriver', {
+            get: () => undefined
+        });
+    });
 
-  // fill information
-//   await page.waitForSelector(emailField, {visible: true})
-//   await page.click(emailField);
-  
+    //console.log(arr_proxy)
+    if(proxy.length>0)
+    await page.authenticate({username: arr_proxy[2], password: arr_proxy[3]});
+
+    await page.goto('https://www.adidas.ca/yeezy');
+    (await browser.pages())[0].close();
+
+    await page.waitForSelector('body', {visible: true});
+    await logger.info('opened adidas page  -', page.url());
+
+    // fill information   await page.waitForSelector(emailField, {visible: true})
+    // await page.click(emailField);
+
 }
 
-try{
-    run();  
+try {
+    run();
 } catch (err) {
-    logger.fatal('In index.js : ' + error(err) , username)
+    logger.fatal('In index.js : ' + error(err), username)
 }
--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-const { Worker } = require('worker_threads')
-const accounts = require('./account.json');
-//todo
-const acc_number = accounts.length;
+const { Worker } = require('worker_threads');
+const fs = require('fs');
+const readline = require('readline');
 
-//let arr_threads = [...Array(acc_number)].fill(0);
+const read = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-for(let i = 0; i< acc_number; i++) {
-    const thread = new Worker(require.resolve('./adidasspoofbrowser.js'), {
-        workerData: { account: accounts[i] /*, proxy: proxy[0]*/}
-    });
 
-    thread.on("message", (msg) => console.log(msg, thread.threadId, accounts[i].email));
-    thread.on("error", (e) => console.log(e));
-    thread.on("exit", (code) => console.log(code))
-}
+const proxy = fs.readFileSync('./config/proxy.txt', 'utf-8')
+    .split('\n')
+    .filter(Boolean);
+
+
+read.question(`You have ${proxy.length} proxies, how many browser you want to open?`, (answer) => {
+    const proxyToPass = proxy.length > 0 ? proxy[i].replace('\r','') : '';
+    for(let i = 0; i < answer; i++) {
+        const thread = new Worker(require.resolve('./adidasspoofbrowser.js'), {
+            workerData: { proxy: proxyToPass}
+        });
+    
+        thread.on("message", (msg) => console.log(msg, thread.threadId, accounts[i].email));
+        thread.on("error", (e) => console.log(e));
+        thread.on("exit", (code) => console.log(code))
+    }
+})
+
+
