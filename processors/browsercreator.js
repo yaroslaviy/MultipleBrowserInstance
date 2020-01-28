023c418689708ea9e94bc062a5bc0926f9a2245d
yaroslaviy <yaselokeev@gmail.com>
2020-01-28 14:02:51 -0500

    added autofill for ys

--- processors/browsercreator.js
+++ processors/browsercreator.js
@@ -1,9 +1,11 @@
 const puppeteer = require('puppeteer-extra');
-const { parentPort, workerData } = require('worker_threads');
+const { workerData } = require('worker_threads');
 const pluginStealth = require('puppeteer-extra-plugin-stealth');
 const Discord = require('discord.js');
 const { discordid, discordtoken } = require('../config/discord');
 const hook = new Discord.WebhookClient(discordid, discordtoken);
+const autofill = require('../config/account.json');
+const cc = require('../config/creditcard.json');
 
 const log4js = require('log4js');
 const logger = log4js.getLogger();
@@ -29,23 +31,26 @@ async function createBrowsers() {
         defaultViewport: null,
         devtools: false,
     });
+
     // open new tab
     const page = await browser.newPage();
 
     const isYeezySupply = /yeezysupply.com/g.test(link);
 
-
+    //proxy auth
     if(proxy.length > 0 && arr_proxy.length > 2) {await page.authenticate({ username: arr_proxy[2], password: arr_proxy[3] });}
 
+    //open page
     await page.goto(link).catch(logger.error('Failed opening the page, check your proxy connection'));
     (await browser.pages())[0].close();
 
+    //wait for page to load
     await page.waitForSelector('body', { visible: true });
-    await logger.info('opened page  -', page.url());
+    logger.info('opened page  -', page.url());
 
     if(isYeezySupply) {
         await page.waitForSelector('.gl-native-dropdown__select-element', { timeout: 0 });
-        await logger.log('PASSED SPLASH');
+        logger.info('PASSED SPLASH');
         await page.bringToFront();
         hook.sendSlackMessage({
             'username': 'MultipleBrowser',
@@ -57,7 +62,26 @@ async function createBrowsers() {
               'ts': Date.now() / 1000,
             }],
           }).catch(console.error);
+        logger.info('before checkout')
+        await page.waitForSelector('#firstName').then(async () =>{
+            logger.info('in checkout')
+            for (let key in autofill){
+                if(key === 'state')
+                    await page.select('select.gl-native-dropdown__select-element', autofill[key])
+                else
+                    await page.type(`#${key}`, autofill[key], {delay: 150})
+            }
+        });
 
+        await page.waitForSelector('#card-number').then(async () =>{
+            logger.info('in checkout')
+            for (let key in cc){
+                if(key === 'state')
+                    await page.select('select.gl-native-dropdown__select-element', cc[key])
+                else
+                    await page.type(`#${key}`, cc[key], {delay: 150})
+            }
+        });
     }
 
 }
